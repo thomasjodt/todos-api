98011cff92c253369b928e58ac5f7e4874d51969
thomasjodt <jodt5200@gmail.com>
2026-05-16 16:37:59 -0500

    feat: add environment configuration and dynamic port handling
    - Introduced environment validation using a schema.
    - Added dynamic handling for `PORT` and `NODE_ENV` via environment variables.
    - Improved server log messages to reflect environment mode.
    - Refactored configuration into `/config` module.

---
+++ src/config/env.ts
@@ -0,0 +1,41 @@
+import { t } from 'elysia'
+import { TypeCompiler } from '@sinclair/typebox/compiler'
+
+// 1. Define the schema.
+/**
+ * Environment variables schema.
+ * this is used to validate the environment variables and ensure they are of the correct type.
+ * this also helps in type safety and autocompletion.
+ */
+const EnvSchema = t.Object({
+  PORT: t.Numeric(),
+  NODE_ENV: t.Union(
+    [t.Literal('development'), t.Literal('production')]
+  ),
+  DATABASE_URL: t.String(),
+  JWT_SECRET: t.String()
+})
+
+// 2. Compile the schema.
+const compiler = TypeCompiler.Compile(EnvSchema)
+
+// 3. Transform and validate the environment variables.
+try {
+  compiler.Decode(process.env)
+}
+
+// 4. If validation fails, extract the errors and throw an error.
+catch (error) {
+  console.error('ERROR: Invalid environment variables')
+  const errors = [...compiler.Errors(error)]
+  errors.forEach((error) => {
+    const path = error.path.replace('/', '')
+    console.error(`  - [${path}]: ${error.message} (Received value: "${error.value}")`)
+  })
+
+  process.exit(1)
+}
+
+// 5. Export the cleaned environment variables with static type safety ready to use.
+const envCleaned = compiler.Decode(process.env)
+export const env = envCleaned
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { env } from './config'
 import { Elysia } from 'elysia'
 import cors from '@elysia/cors'
 import openapi, { fromTypes } from '@elysia/openapi'
@@ -5,8 +6,9 @@ import openapi, { fromTypes } from '@elysia/openapi'
 const app = new Elysia()
   .use(cors())
   .use(openapi({ references: fromTypes() }))
-  .get('/health', () => 'ok').listen(3000)
+  .get('/health', () => 'ok')
+  .listen(env.PORT)
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running in ${env.NODE_ENV} mode at ${app.server?.hostname}:${app.server?.port}`
 )
