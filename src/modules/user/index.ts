58263db670f1ba439671aa147b2169ae1ebd8ee2
thomasjodt <jodt5200@gmail.com>
2026-05-23 16:26:18 -0500

    feat(user): export user service and repository from module

--- src/modules/user/index.ts
+++ src/modules/user/index.ts
@@ -1,2 +1,4 @@
 export { users } from './user.schema'
+export { userService } from './user.service'
 export { userController } from './user.controller'
+export { userRepository } from './user.repository'
