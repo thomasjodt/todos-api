1571ae1ab6535eb668fe07e6ff4b34aaf866b9ac
thomasjodt <jodt5200@gmail.com>
2026-05-23 16:22:08 -0500

    feat(auth): add refresh token error classes and update status code for existing error
    - Introduced `MissingRefreshTokenError`, `InvalidRefreshTokenError`, and `ExpiredRefreshTokenError` for handling refresh token-related issues.
    - Updated `AlreadyExistingUserError` to use HTTP status code 409.

--- src/modules/auth/auth.errors.ts
+++ src/modules/auth/auth.errors.ts
@@ -1,19 +1,37 @@
 import { AppError } from '@/shared/errors/app.error'
 
 export class InvalidCredentialsError extends AppError {
-  constructor(message = "Invalid credentials") {
+  constructor(message = "Invalid credentials.") {
     super(message, 400, "INVALID_CREDENTIALS_ERROR")
   }
 }
 
 export class AlreadyExistingUserError extends AppError {
   constructor(message = "The username is already taken.") {
-    super(message, 400, "ALREADY_EXISTING_USER_ERROR")
+    super(message, 409, "ALREADY_EXISTING_USER_ERROR")
   }
 }
 
 export class UserCreationError extends AppError {
-  constructor(message = "Failed to create user") {
+  constructor(message = "Failed to create user.") {
     super(message, 500, "USER_CREATION_ERROR")
+  }
+}
+
+export class MissingRefreshTokenError extends AppError {
+  constructor(message = "Missing refresh token.") {
+    super(message, 401, "MISSING_REFRESH_TOKEN_ERROR")
+  }
+}
+
+export class InvalidRefreshTokenError extends AppError {
+  constructor(message = "Invalid refresh token.") {
+    super(message, 401, "INVALID_REFRESH_TOKEN_ERROR")
+  }
+}
+
+export class ExpiredRefreshTokenError extends AppError {
+  constructor(message = "Expired Refresh token.") {
+    super(message, 401, "REFRESH_TOKEN_EXPIRED_ERROR")
   }
 }
