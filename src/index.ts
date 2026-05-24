298c17680183c98bd80bddf94f7d53c186c28e2c
thomasjodt <jodt5200@gmail.com>
2026-05-24 15:34:29 -0500

    refactor(router): replace individual controllers with modular routers

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,16 @@ import { env } from './config'
 import { Elysia } from 'elysia'
 import cors from '@elysia/cors'
 import openapi, { fromTypes } from '@elysia/openapi'
-import { userController } from '@/modules/user'
-import { authController } from '@/modules/auth'
+
+import { protectedRouter, publicRouter } from './router'
 import { globalErrorHandler } from '@/shared/middleware/error.middleware'
-import { AppError } from '@/shared/errors/app.error'
 
 const app = new Elysia()
   .use(cors())
   .use(openapi({ references: fromTypes() }))
   .onError(globalErrorHandler)
-  .use(authController)
-  .use(userController)
+  .use(publicRouter)
+  .use(protectedRouter)
   .listen(env.PORT)
 
 console.log(
