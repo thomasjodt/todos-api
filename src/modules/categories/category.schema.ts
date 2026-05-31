4a505dbf2a7b19212181fea434f449ecd5fd27be
thomasjodt <jodt5200@gmail.com>
2026-05-30 22:42:42 -0500

    feat(category): Add category schema

---
+++ src/modules/categories/category.schema.ts
@@ -0,0 +1,17 @@
+import { pgTable, text, timestamp, uniqueIndex, uuid } from 'drizzle-orm/pg-core'
+
+export const categories = pgTable('categories', {
+  id: uuid('id').defaultRandom().primaryKey(),
+
+  userId: uuid('user_id')
+    .notNull(),
+
+  name: text('name')
+    .notNull(),
+
+  color: text('color'),
+
+  createdAt: timestamp('created_at').defaultNow().notNull()
+}, (table) => ({
+  userCategoryUnique: uniqueIndex('categories_user_name_idx').on(table.userId, table.name)
+}))
