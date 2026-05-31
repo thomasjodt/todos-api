7f2ec61e7d8ffe292c9e94e7d30b3dc6b3a20a75
thomasjodt <jodt5200@gmail.com>
2026-05-30 20:29:24 -0500

    feat(frequent-item): Add frequent-item schema

---
+++ src/modules/frequent-item/frequent-item.schema.ts
@@ -0,0 +1,18 @@
+import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core'
+import { users } from '@/modules/user'
+
+export const frequentItems = pgTable('frequent_items', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  userId: uuid('user_id')
+    .notNull()
+    .references(() => users.id, {
+      onDelete: 'cascade'
+    }),
+  categoryId: uuid('category_id'),
+    // .references(() => categories.id, { onDelete: 'cascade' }),
+  name: text('name').notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull()
+})
+
+export type FrequentItem = typeof frequentItems.$inferSelect
+export type NewFrequentItem = typeof frequentItems.$inferInsert
